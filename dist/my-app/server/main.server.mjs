61d2d19eeacb41ca0659240cb546281771cc424d
TziviRotbard <tzivi65@gmail.com>
2026-04-19 00:42:54 +0300

    Final build files for deployment

---
+++ dist/my-app/server/main.server.mjs
@@ -0,0 +1,9 @@
+import './polyfills.server.mjs';
+import {
+  main_server_default
+} from "./chunk-APPMGSTZ.mjs";
+import "./chunk-K3INNTAY.mjs";
+import "./chunk-SDCGKQVP.mjs";
+export {
+  main_server_default as default
+};
