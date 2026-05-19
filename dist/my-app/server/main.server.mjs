647f583fcb4a264a3661e88f95e09864f7d9d160
TziviRotbard <tzivi65@gmail.com>
2026-05-20 01:17:16 +0300

    fix placeholder image route for netfree

--- dist/my-app/server/main.server.mjs
+++ dist/my-app/server/main.server.mjs
@@ -1,7 +1,7 @@
 import './polyfills.server.mjs';
 import {
   main_server_default
-} from "./chunk-Q2M6XSZT.mjs";
+} from "./chunk-HUSX4AHM.mjs";
 import "./chunk-K3INNTAY.mjs";
 import "./chunk-SDCGKQVP.mjs";
 export {
--- dist/my-app/server/server.mjs
+++ dist/my-app/server/server.mjs
@@ -1,7 +1,7 @@
 import './polyfills.server.mjs';
 import {
   main_server_default
-} from "./chunk-Q2M6XSZT.mjs";
+} from "./chunk-HUSX4AHM.mjs";
 import {
   APP_BASE_HREF,
   SERVER_CONTEXT,
